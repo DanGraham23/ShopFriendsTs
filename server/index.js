d36d1cd42cd5339ce261bebfd05cb140ad2b2501
DanGraham23 <d20grah@siena.edu>
2023-05-10 22:22:39 -0400

    add build files

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-
+const path = require('path');
 
 const userRoutes = require("./routes/userRoutes");
 const itemRoutes = require("./routes/itemRoutes");
@@ -19,6 +19,14 @@ app.use("/api/auth", userRoutes);
 app.use("/api/item", itemRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/checkout", checkoutRoutes);
+
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
+
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`server connected on port:${process.env.SERVER_PORT}`);
