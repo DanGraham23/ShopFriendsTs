f247910a399d156563cad5c40141d1b119afc883
DanGraham23 <d20grah@siena.edu>
2023-05-11 18:43:26 -0400

    fix minor bugs

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const path = require('path');
 
 const userRoutes = require("./routes/userRoutes");
 const itemRoutes = require("./routes/itemRoutes");
@@ -19,14 +18,6 @@ app.use("/api/auth", userRoutes);
 app.use("/api/item", itemRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/checkout", checkoutRoutes);
-
-
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-});
-
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`server connected on port:${process.env.SERVER_PORT}`);
